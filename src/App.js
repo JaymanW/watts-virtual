24ea5c53d26a827c3f70cfe6f03b28f8f4dca057
Jayden W <jaymanw2000@gmail.com>
2021-06-29 13:30:25 -0500

    header mobile init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import React, { useState, useEffect } from 'react'
 import './App.scss';
 
+import Header from './components/Header'
+
 function App() {
+  const [windowWidth, setWindowWidth] = useState(null);
+
+  useEffect(() => {
+    updateWindowWidth();
+    window.addEventListener("resize", updateWindowWidth);
+    return () => window.removeEventListener("resize", updateWindowWidth);
+  }, []);
+
+  // useEffect(() => {
+  //   console.log(windowWidth)
+  // }, [windowWidth])
+
+  const updateWindowWidth = () => {
+    const width = window.innerWidth;
+    setWindowWidth(width);
+    }
+  
   return (
     <div className="App">
-    
+      <Header windowWidth={windowWidth} />
+      {/* LANDING */}
+      {/* ABOUT */}
+      {/* SERVICES */}
+      {/* OUR WORK */}
+      {/* CONTACT */}
+      {/* FOOTER */}
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './Header.scss'
+
+import Hamburger from 'hamburger-react'
+
+function Header(props) {
+    return (
+        <div className="Header">
+            <div className="Header-wrapper">
+                <div className="logo-cnt">
+                    <img src="./wvLogo.png" />
+                    <a href="#">
+                        <h1>Watts Virtual</h1>
+                    </a>
+                </div>
+                {
+                    props.windowWidth >= 768 ?
+                    <nav>
+                        <ul className="nav-menu">
+                            <li>
+                                <a href="#">About Us</a>
+                            </li>
+                            <li>
+                                <a href="#">Our Features</a>
+                            </li>
+                            <li>
+                                <a href="#">Our Work</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    :
+                    <Hamburger 
+                        size="30"
+                        color='#000'
+                        // onToggle={() => props.onBurger()}
+                        // toggled={props.isOpen}
+                    />
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Header
