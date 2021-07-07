a1cb0da0c6ac382bf24f174df63cb28b98051197
Jayden W <jaymanw2000@gmail.com>
2021-07-07 12:22:44 -0500

    work tablet

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -23,7 +23,10 @@ function Work() {
                         <div className="work-item-text">
                             <h3>Value Add Services</h3>
                             <p><a href="#" target="_blank">Value Add Services</a> is a kitchen remodeling company servicing the south-eastern Minnesota region. We collaborated with them to create a website showcasing their beautiful work and drive business directly to their email.</p>
-                            <a href="#" target="_blank" className="work-btn">CHECK IT OUT</a>
+                            <a href="#" target="_blank" className="work-btn">
+                                <p>CHECK IT OUT</p>
+                                <img src="rightArrow20.png" />
+                            </a>
                         </div>
                     </div>
                     <div className="work-item">
@@ -31,7 +34,10 @@ function Work() {
                         <div className="work-item-text">
                             <h3>sliderpuzzle.io</h3>
                             <p><a href="#" target="_blank">Sliderpuzzle.io</a> is a modern web adaptation of the classic sliding puzzle game. Complete with smooth gameplay, account creation, and live leaderboards, this application demonstrates our ability to seemlessly integrate front-end and back-end web technologies.</p>
-                            <a href="#" target="_blank" className="work-btn">CHECK IT OUT</a>
+                            <a href="#" target="_blank" className="work-btn">
+                                <p>CHECK IT OUT</p>
+                                <img src="rightArrow20.png" />
+                            </a>
                         </div>
                     </div>    
                  </div>
